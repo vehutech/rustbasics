7c7d947438aa3787d5b19883ea2151f6aa8d98bc
D Vehu Alonge <84502425+vehutech@users.noreply.github.com>
2025-12-02 09:20:06 +0100

    Feat:

--- lesson4/src/main.rs
+++ lesson4/src/main.rs
@@ -2,14 +2,49 @@
 
 // Every function begins with the reserved word fn which stands for function, the name of the function, followed by paranthesis then a code block.
 
-// FUnction or variable names must be written in snake case - hello_world or kebab case -  hello-world
+// Function or variable names must be written in snake case - hello_world or kebab case -  hello-world
 
-// Rust supports hoisting - a programming style in which function can be called from anywhere, even before its definition.
+// Rust supports hoisting - a programming style in which function can be called from anywhere, even before its definition. Hoisting exists in javascript too
 
 fn main(){
     hello_woorld();
+    tell_height(50);
+    human_id("Vehu", 30, 170.0);
+
+    let price = 5;
+    let qty = 10;
+    let _x  = {
+        price * qty
+    };
+
+    println!("Result of {} * {} is: {}", price, qty, _x);
+
+    let y = add(4, 6);
+    println!("The value of y is {}", y);
+    println!("Value from function 'add' is: {}", add(4, 6))
 }
 
 fn hello_woorld(){
     println!("Helo Rust!");
+}
+
+// you can insert input values
+
+fn tell_height(height: i32) {
+    println!("My height is: {}cm", height);
+}
+
+fn human_id(name: &str, age: u32, height: f32) {
+    println!("My name is {},\nI am {}, years old\nMy height is {}cm", name, age, height)
+}
+
+// Expressions and Statements
+
+// Expressions are anything that return a value.
+// Statements are anything that does not return a value
+
+
+// Functions retunrs values too
+fn add(a: i32, b:i32) -> i32 {
+    a+b
 }
